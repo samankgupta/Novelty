6bf02018a5331a026356624208c30fae9712f294
Samank Gupta <samankgupta@gmail.com>
2021-12-15 02:27:59 +0530

    Game playing complete

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -3,6 +3,7 @@ import Chessboard from "chessboardjsx";
 import useSound from 'use-sound';
 import pieceSound from '../assets/piecesound.wav';
 import { Helmet } from "react-helmet"
+import { useNavigate } from 'react-router-dom';
 import { NotificationContainer, NotificationManager } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 
@@ -10,11 +11,13 @@ const Chess = require("chess.js");
 
 function Game() {
 
+    const navigate = useNavigate();
     const [play] = useSound(pieceSound);
 
     const [account1, setAccount1] = React.useState({ 'account': '', 'amount': '' })
     const [account2, setAccount2] = React.useState({ 'account': '', 'amount': '' })
     const [game, setGame] = useState();
+    const [toPlay, setToPlay] = useState("White");
     const [movesList, setMovesList] = useState([["", ""]]);
     const [totalMoves, setTotalMoves] = useState(0);
 
@@ -25,7 +28,6 @@ function Game() {
     }, [])
 
     const [position, setPosition] = useState("start");
-
 
     const onDropMove = ({ sourceSquare, targetSquare }) => {
         const move = game.move({
@@ -47,11 +49,17 @@ function Game() {
                 setTotalMoves(totalMoves + 1)
             }
             setMovesList(tempList);
+            if (move.color === "w")
+                setToPlay("Black")
+            else
+                setToPlay("White")
         }
 
         setPosition(game.fen());
-        if (game.game_over())
+        if (game.game_over()) {
             gameOver(move)
+            setToPlay("Over")
+        }
     };
 
     const sendToAccount1 = async () => {
@@ -64,8 +72,14 @@ function Game() {
             }, 6900);
         }
         else {
-            const res = await web3.eth.sendTransaction({ to: account1.account, from: account2.account, value: web3.utils.toWei(account1.amount) });
-            console.log(res)
+            const res = await web3.eth.sendTransaction({ to: account1.account, from: account2.account, value: web3.utils.toWei(account2.amount) });
+            if (res) {
+                NotificationManager.success('Thanks for Playing!', 'Transaction Successful!')
+                setTimeout(() => {
+                    if (window.confirm('Navigating you to the Stake Page.'))
+                        navigate("/stake")
+                }, 2000);
+            }
         }
     }
 
@@ -75,12 +89,38 @@ function Game() {
         if (accounts[0] !== account1.account) {
             alert('Change to the other account from Metamask.')
             setTimeout(() => {
-                sendToAccount1();
+                sendToAccount2();
             }, 6900);
         }
         else {
-            const res = web3.eth.sendTransaction({ to: account2.account, from: account1.account, value: web3.utils.toWei(account2.amount) });
-            console.log(res)
+            const res = await web3.eth.sendTransaction({ to: account2.account, from: account1.account, value: web3.utils.toWei(account1.amount) });
+            if (res) {
+                NotificationManager.success('Thanks for Playing!', 'Transaction Successful!')
+                setTimeout(() => {
+                    if (window.confirm('Navigating you to the Stake Page.'))
+                        navigate("/stake")
+                }, 2000);
+            }
+        }
+    }
+
+    const resign = () => {
+        if (window.confirm(`${toPlay} - Are you sure to Resign?`)) {
+            if (toPlay === "Black") {
+                NotificationManager.success('White won by resignation.', 'Congratulations!');
+                setTimeout(() => {
+                    if (window.confirm(`${account2.account} will now be transferring ${account2.amount} Tokens to ${account1.account}`))
+                        sendToAccount1();
+                }, 2500);
+            }
+            else {
+                NotificationManager.success('Black won by resignation.', 'Congratulations!');
+                setTimeout(() => {
+                    if (window.confirm(`${account1.account} will now be transferring ${account1.amount} Tokens to ${account2.account}`))
+                        sendToAccount2();
+                }, 3000);
+            }
+            setToPlay("Over")
         }
     }
 
@@ -123,12 +163,12 @@ function Game() {
                     </div>
                 </div>
                 <div className="pt-6">
-                    <div className="overflow-y-auto max-h-96 no-scrollbar mt-10 ml-20">
-                        <table className="table-auto border-collapse ml-32 w-1/2">
+                    <div className="overflow-y-auto h-96 no-scrollbar mt-6 ml-28 bg-gray-900 mx-48">
+                        <table className="table-auto border-collapse w-full">
                             <tbody className="text-sm font-normal text-slate-100">
-                                {movesList ? movesList.map((mov, index) =>
+                                {movesList[0][0] !== '' ? movesList.map((mov, index) =>
                                     mov[0] !== "" ?
-                                        <tr className="hover:bg-slate-900 border-b border-slate-200 py-10">
+                                        <tr className="hover:bg-slate-800 border-b border-slate-200 py-10">
                                             <td className="px-2 py-2">{index + 1}.</td>
                                             <td className="px-2 py-2">{mov[0]}</td>
                                             <td className="px-2 py-2">{mov[1]}</td>
@@ -136,9 +176,15 @@ function Game() {
                                         :
                                         <></>
 
-                                ) : ''}
+                                ) : <div className="text-xl mt-4 text-slate-100">Moves will be displayed here</div>}
                             </tbody>
                         </table>
+                    </div>
+                    <div className="mt-16 mr-12">
+                        <button onClick={resign} className="text-white text-lg font-medium rounded-xl px-16 py-3 bg-gradient-to-r from-amber-600 to-amber-300 hover:from-amber-700 hover:to-amber-500 hover:shadow-2xl">Resign</button>
+                    </div>
+                    <div className="mt-12 mr-12">
+                        <div className="text-md mt-4 text-slate-500 hover:text-slate-400">{toPlay !== "Over" ? `${toPlay} To Play` : 'Game Over'}</div>
                     </div>
                 </div>
             </div>
